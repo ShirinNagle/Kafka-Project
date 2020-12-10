49de2a9b8f85fb1e7d3b18c01c695bfaec1fdc94
ShirinNagle <G00363458@gmit.ie>
2020-12-10 17:23:38 +0000

    All transactions go to correct topics, some methods unused

--- bank-api-service/src/main/java/Application.java
+++ bank-api-service/src/main/java/Application.java
@@ -1,4 +1,5 @@
 import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.LongSerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
@@ -9,20 +10,34 @@ import java.util.concurrent.ExecutionException;
  */
 public class Application {
 
+    private static final String TOPIC = "valid-transactions";//
+    private static final String TOPIC1 = "suspicious-transactions";//
+    private static final String TOPIC2 = "high-value";//
+    //only need 9092 but adding the other local hosts in case it goes down
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
+    private static final double HIGH_VALUE_AMT = 1000.00;
+
+
     public static void main(String[] args) {
         IncomingTransactionsReader incomingTransactionsReader = new IncomingTransactionsReader();
         CustomerAddressDatabase customerAddressDatabase = new CustomerAddressDatabase();
+        //Transaction transaction = new Transaction();
+        Application kafkaApplication = new Application();
+        Producer<String, Transaction> kafkaProducer = kafkaApplication.createKafkaProducer(BOOTSTRAP_SERVERS);
 
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        try {
+            processTransactions(incomingTransactionsReader, customerAddressDatabase,kafkaProducer);
+        } catch (ExecutionException | InterruptedException e) {
+            e.printStackTrace();
+        } finally{
+            kafkaProducer.flush();//clears the producer
+            kafkaProducer.close();//close any network connections formed
+        }
+
     }
-
-    public void processTransactions(IncomingTransactionsReader incomingTransactionsReader,
+    public static void processTransactions(IncomingTransactionsReader incomingTransactionsReader,
                                     CustomerAddressDatabase customerAddressDatabase,
                                     Producer<String, Transaction> kafkaProducer) throws ExecutionException, InterruptedException {
-
-
         // Retrieve the next transaction from the IncomingTransactionsReader
         // For the transaction user, get the user residence from the UserResidenceDatabase
         // Compare user residence to transaction location.
@@ -30,15 +45,56 @@ public class Application {
         // location match or not.
         // Print record metadata information
 
-        //*****************
-        // YOUR CODE HERE
-        //*****************
+        int i = 0;
+        while (incomingTransactionsReader.hasNext()) {
+            Transaction transaction = incomingTransactionsReader.next();
+
+            String key = transaction.getUser();
+            String userResidence = customerAddressDatabase.getUserResidence(transaction.getUser());
+            if(transaction.getTransactionLocation().equalsIgnoreCase(userResidence))
+            {
+              ProducerRecord<String, Transaction> record = new ProducerRecord<>(TOPIC,key, transaction);//not sure this is correct
+              RecordMetadata recordMetadata = kafkaProducer.send(record).get();//futures async call
+               System.out.println(String.format("Record with (keys: %s, value: %s) was sent to (partition: %d, offset: %d, topic: %s",
+                      record.key(),
+                      record.value(), recordMetadata.partition(), recordMetadata.offset(),record.topic()));
+            }
+            else {
+                ProducerRecord<String, Transaction> record = new ProducerRecord<>(TOPIC1,key, transaction);
+                RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+                //key is user
+                System.out.println(String.format("Record with (keys: %s, value: %s) was sent to (partition: %d, offset: %d, topic: %s", record.key(),
+                        record.value(), recordMetadata.partition(), recordMetadata.offset(), record.topic()));
+            }
+            //decided to have a separate if statement for the higher value topic as Acc mgr needs to be notified for all valid amts
+            //considered a nested if, but a separate if seemed the most straight forward.
+            if(transaction.getTransactionLocation().equalsIgnoreCase(userResidence) && transaction.getAmount() > HIGH_VALUE_AMT){
+                ProducerRecord<String, Transaction> record = new ProducerRecord<>(TOPIC2,key, transaction);
+                RecordMetadata recordMetadata = kafkaProducer.send(record).get();
+                //key is user
+                System.out.println(String.format("Record with (keys: %s, value: %s) was sent to (partition: %d, offset: %d, topic: %s", record.key(),
+                        record.value(), recordMetadata.partition(), recordMetadata.offset(), record.topic()));
+            }
+
+        }
     }
 
-//    public Producer<String, Transaction> createKafkaProducer(String bootstrapServers) {
+    public Producer<String, Transaction> createKafkaProducer(String bootstrapServers) {
     //*****************
     // YOUR CODE HERE
     //*****************
-//    }
+       Properties properties= new Properties();
+       properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);//key = BOOTSTRAP_SERVERS, value = bootstrapServers
+       //Give producer a name
+        properties.put(ProducerConfig.CLIENT_ID_CONFIG, "transactions-producer");//uniquely id each individual producer
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());//any kind of object can be used as an key or value, need to tell Kafka how to serialise those objects into byte streams
+       properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, Transaction.TransactionSerializer.class.getName());
+
+       return new KafkaProducer<String, Transaction>(properties);
+
+
+
+
+  }
 
 }
--- reporting-service/src/main/java/Application.java
+++ reporting-service/src/main/java/Application.java
@@ -1,26 +1,72 @@
-import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.*;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Properties;
 
 public class Application {
+    private static final List <String> TOPICS = Arrays.asList("valid-transactions", "suspicious-transactions");//may change this
+    //private static final String TOPIC1 = "suspicious-transactions";
+    private static final String BOOTSTRAP_SERVERS = "localhost:9092,localhost:9093,localhost:9094";
 
     public static void main(String[] args) {
         //*****************
         // YOUR CODE HERE
         //*****************
+        Transaction transaction = new Transaction();
+        Application kafkaConsumerApp = new Application();
+
+        String consumerGroup = "reporting-service";
+        if (args.length == 1) {//if user passes in cmd line parameter use this otherwise use default above
+            consumerGroup = args[0];
+        }
+
+        System.out.println("Consumer is part of consumer group " + consumerGroup);
+
+        Consumer<String, Transaction> kafkaConsumer = kafkaConsumerApp.createKafkaConsumer(BOOTSTRAP_SERVERS, consumerGroup);
+
+        kafkaConsumerApp.consumeMessages(TOPICS, kafkaConsumer);
+        //recordTransactionForReporting(String , transaction);//need to pass in correct argument not entirely happy with list above
+        System.out.println("Placeholder for method, when I work out correct list items");
     }
 
     public static void consumeMessages(List<String> topics, Consumer<String, Transaction> kafkaConsumer) {
         //*****************
         // YOUR CODE HERE
         //*****************
+        //kafkaConsumer.subscribe(Collections.singletonList(topics));
+        kafkaConsumer.subscribe(Collections.synchronizedList(topics));//not sure about synchronized list
+
+        while(true){
+            ConsumerRecords<String, Transaction> consumerRecords = kafkaConsumer.poll(Duration.ofSeconds(1));
+
+            if(consumerRecords.isEmpty()){
+
+            }
+            for(ConsumerRecord<String, Transaction> record: consumerRecords){
+                System.out.println(String.format("Received record (key: %s, value: %s, partition: %d, offset: %d",
+                        record.key(),record.value(), record.partition(), record.offset()));
+            }
+            kafkaConsumer.commitAsync();
+        }
     }
 
-//    public static Consumer<String, Transaction> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
+    public static Consumer<String, Transaction> createKafkaConsumer(String bootstrapServers, String consumerGroup) {
         //*****************
         // YOUR CODE HERE
         //*****************
-//    }
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);//how the consumer can access the cluster
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer .class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, Transaction.TransactionDeserializer.class.getName());
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, consumerGroup);
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);//turning auto commit off.
+
+      return new KafkaConsumer<>(properties);
+   }
 
     private static void recordTransactionForReporting(String topic, Transaction transaction) {
         // Print transaction information to the console
@@ -29,6 +75,13 @@ public class Application {
         //*****************
         // YOUR CODE HERE
         //*****************
+        System.out.println("All records go here");
+        if(TOPICS.contains("valid-transactions")){
+            System.out.println("valid transaction for user %s"+ transaction.getUser());
+        }
+        else{
+            System.out.println("invalid transaction for user %s" + transaction.getUser());
+        }
     }
 
 }
